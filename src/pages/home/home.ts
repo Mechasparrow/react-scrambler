a994e25663e708af15366624a248b4b23717d778
Michael Navazhylau <mikipux7@gmail.com>
2016-12-05 18:44:52 -0600

    Added timer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 
 import { NavController } from 'ionic-angular';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'page-home',
@@ -10,11 +13,29 @@ export class HomePage {
 
   private scramble:string = "";
   private scramblelength: number = 12;
-
+  private ticks:number = 0;
+  private filteredtick:string = "";
+  private subscription:any;
   public moves:any = ["U", "L", "F", "R", "B", "D"];
 
   constructor(public navCtrl: NavController) {
+  }
 
+  starttimer(){
+    let timer = Observable.timer(0, 1000);
+    this.subscription = timer.subscribe(t => {
+      this.ontick(t);
+    });
+  }
+
+  stoptimer() {
+    this.subscription.unsubscribe();
+  }
+
+  ontick(t) {
+    this.filteredtick = moment("2015-01-01").startOf('day')
+    .seconds(t)
+    .format('H:mm:ss');
   }
 
   scrambleclick(){
